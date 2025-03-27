ab5c4feeed3c7106ebb12fa341083aae343a0889
Eduardo-Tresmann <contatoeduardotresmann@gmail.com>
2025-03-27 18:15:36 -0400

    Update

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,22 @@
-import {View, Text} from "react-native"
+import {View, Text, StyleSheet} from "react-native"
 
 export default function Index(){
     return (
-        <View>
-            <Text>Hello World!</Text>
+        <View style = {Style.contaniner}>
+            <Text style = {Style.text}>Hello world!</Text>
         </View>
     )
 }
+
+const Style = StyleSheet.create({
+    contaniner: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#111"
+    },
+    text: {
+        color: "red",
+        fontSize: 30,
+    }
+})
